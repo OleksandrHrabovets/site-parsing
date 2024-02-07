e88e8f332993c87ab89bfb1dcea403ffc326dbaf
Oleksandr Hrabovets <alex6754035@gmail.com>
2024-02-07 10:16:04 +0200

    add parsing product list

--- src/main/java/ua/oh/siteparsing/service/FashionistaWebParser.java
+++ src/main/java/ua/oh/siteparsing/service/FashionistaWebParser.java
@@ -2,6 +2,7 @@ package ua.oh.siteparsing.service;
 
 import jakarta.annotation.PostConstruct;
 import java.io.IOException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
@@ -14,10 +15,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class FashionistaWebParser {
+
   @Value("${app.url}")
   private String url;
 
-  @PostConstruct
+  //  @PostConstruct
   public Elements parseUrlByClass() throws IOException {
 
     Document document = Jsoup.connect(url).get();
@@ -28,15 +30,43 @@ public class FashionistaWebParser {
 
   }
 
-  @PostConstruct
+  //  @PostConstruct
   public Elements parseUrlByAttribute() throws IOException {
 
     Document document = Jsoup.connect(url).get();
     String attr = "href";
     Elements elementsByClass = document.getElementsByAttribute(attr);
-    elementsByClass.forEach(element -> log.info("Element by attribute '{}': {}", attr,  element));
+    elementsByClass.forEach(element -> log.info("Element by attribute '{}': {}", attr, element));
     return elementsByClass;
 
   }
 
+  @PostConstruct
+  public List<ProductRecord> parseProductsList() throws IOException {
+
+    Document document = Jsoup.connect(url).get();
+    String query = "div.ajax_block_product";
+    Elements elementsBySelect = document.select(query);
+
+    return elementsBySelect.stream().map(element -> {
+          String idString = element.getElementsByAttribute("data-id-product").first()
+              .attr("data-id-product");
+          String title = element.getElementsByClass("product-title").first().text();
+          String url = element.getElementsByClass("product-title").first().childNodes().get(0)
+              .attr("href");
+          String description = element.getElementsByClass("product-description-short").first().text();
+          String priceString = element.getElementsByClass("price").first().childNodes().get(2)
+              .attr("content");
+          ProductRecord productRecord = new ProductRecord(Long.parseLong(idString), title, description,
+              url, Double.parseDouble(priceString));
+          log.info("Product {}", productRecord);
+          return productRecord;
+        }
+    ).toList();
+
+  }
+
+  public record ProductRecord(Long id, String title, String Description, String url, Double price) {
+
+  }
 }
